adb55e04fd02844fdf8a075600c96b397d5284cc
Alexandre Gomes <alxgomes@gmail.com>
2023-07-14 02:25:30 -0300

    Criado o endpoint para buscar game por id

--- dslist/src/main/java/br/com/semog4/dslist/controllers/GameController.java
+++ dslist/src/main/java/br/com/semog4/dslist/controllers/GameController.java
@@ -4,12 +4,14 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.semog4.dslist.dto.GameDTO;
 import br.com.semog4.dslist.dto.GameMinDTO;
-import br.com.semog4.dslist.entities.Game;
 import br.com.semog4.dslist.services.GameService;
+import jakarta.websocket.server.PathParam;
 
 @RestController
 @RequestMapping(value = "/games")  // recurso games
@@ -17,12 +19,20 @@ public class GameController {
 
 	@Autowired
 	private GameService gameService;
+		
+	//criando os endpoints para game
 	
 	@GetMapping
 	public List<GameMinDTO> findAll(){
 		
 		List<GameMinDTO> result = gameService.findAll();
 		return result;
+	}
+	
+	@GetMapping (value = "/{id}")
+	public GameDTO findById(@PathVariable Long id) {
+		   GameDTO dto = gameService.findById(id);
+	       return dto;
 	}
 	
 	
--- dslist/src/main/java/br/com/semog4/dslist/services/GameService.java
+++ dslist/src/main/java/br/com/semog4/dslist/services/GameService.java
@@ -4,7 +4,9 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import br.com.semog4.dslist.dto.GameDTO;
 import br.com.semog4.dslist.dto.GameMinDTO;
 import br.com.semog4.dslist.entities.Game;
 import br.com.semog4.dslist.repositories.GameRepository;
@@ -22,14 +24,28 @@ public class GameService {
 	@Autowired
 	private GameRepository gameRepository;
 	
-	
-	//busca todos os game min
+
+	//busca todos os game min	
+	//anotação garante que vai ocorrer a transação com o banco e que é somente de leitura
+	//isso torna o acesso mais rápido. Garantir que a transação não vai bloquear o banco
+	//para realização da operação porque não é de escrita somente de leitura.
+	@Transactional(readOnly = true)	
 	public List<GameMinDTO> findAll(){
 		List<Game> listaDosGamesComTodosCampos = gameRepository.findAll();
 		List<GameMinDTO> dto = listaDosGamesComTodosCampos.stream().map(game -> new GameMinDTO(game)).toList(); 
 		return dto;
 	}
 	
+	//busca um game por id
+	//anotação garante que vai ocorrer a transação com o banco e que é somente de leitura
+	//isso torna o acesso mais rápido. Garantir que a transação não vai bloquear o banco
+	//para realização da operação porque não é de escrita somente de leitura.
+	@Transactional(readOnly = true)	
+	public GameDTO findById(Long id) {
+		Game game = gameRepository.findById(id).get();
+		GameDTO dto = new GameDTO(game);
+		return dto;
+	}
 	
 	
 }
