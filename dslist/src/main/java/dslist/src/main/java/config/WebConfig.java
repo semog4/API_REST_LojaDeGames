9d67560c7463fc8a7b7d61f5cd822e377b02793a
Alexandre Gomes <alxgomes@gmail.com>
2023-07-18 17:12:43 -0300

    COR_ORIGINS e classe WebConfig

---
+++ dslist/src/main/java/dslist/src/main/java/config/WebConfig.java
@@ -0,0 +1,31 @@
+package dslist.src.main.java.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * Lê o parametro cors.origins que está no arquivo applicatio.properties
+ * Somente os endereços configurados na variável poderá acessar a API
+ */
+
+
+@Configuration
+public class WebConfig {
+
+	@Value("${cors.origins}")
+	private String corsOrigins;
+	
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**").allowedMethods("*").allowedOrigins(corsOrigins);
+			}
+		};
+	}
+	
+}
